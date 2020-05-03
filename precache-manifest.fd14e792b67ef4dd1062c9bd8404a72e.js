e60e87738a0052183a5479091f28a4b33ead82c3
veeresh-git <veereshv537@gmail.com>
2020-05-03 18:31:45 +0530

    Updates

---
+++ precache-manifest.fd14e792b67ef4dd1062c9bd8404a72e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb4fb46d641ac636e604429206090e3",
+    "url": "/fashionworldapp/index.html"
+  },
+  {
+    "revision": "236a5a0f31d99fc624c7",
+    "url": "/fashionworldapp/static/css/main.32c76674.chunk.css"
+  },
+  {
+    "revision": "8d3e43b5233e340099be",
+    "url": "/fashionworldapp/static/js/2.bc6309ef.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/fashionworldapp/static/js/2.bc6309ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236a5a0f31d99fc624c7",
+    "url": "/fashionworldapp/static/js/main.6fadab99.chunk.js"
+  },
+  {
+    "revision": "2324e451aef1b4a980ae",
+    "url": "/fashionworldapp/static/js/runtime-main.34379542.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/fashionworldapp/static/media/crown.19ef7b17.svg"
+  }
+]);
